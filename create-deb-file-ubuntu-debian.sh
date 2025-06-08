38b93dfb00cdc83e6d3a30d7ec0c733767013e31
amidevous1 <amidevous@gmail.com>
2025-06-08 17:20:05 +0200

    Update create-deb-file-ubuntu-debian.sh

--- create-deb-file-ubuntu-debian.sh
+++ create-deb-file-ubuntu-debian.sh
@@ -14,11 +14,14 @@ sudo apt-get -y install libtinyxml-dev
 rm -f desmume*
 wget https://github.com/TASEmulators/desmume/archive/refs/tags/release_0_9_13.tar.gz -O desmume_0.9.13.orig.tar.gz
 tar -xvf desmume_0.9.13.orig.tar.gz
-wget https://github.com/amidevous1/desmume/archive/refs/heads/ubuntu-debian.tar.gz -O desmume_0.9.13-4.debian.tar.gz
+#wget https://github.com/amidevous1/desmume/archive/refs/heads/ubuntu-debian.tar.gz -O desmume_0.9.13-4.debian.tar.gz
+wget https://github.com/amidevous1/desmume/archive/23de6bb7b34c5f941eaa8ec19c89eb83df414ecd.tar.gz -O desmume_0.9.13-4.debian.tar.gz
 tar -xvf desmume_0.9.13-4.debian.tar.gz
 rm -f desmume_0.9.13-4.debian.tar.gz
 mkdir desmume-release_0_9_13/debian
-cp -R desmume-ubuntu-debian/debian/* desmume-release_0_9_13/debian/
-rm -rf desmume-ubuntu-debian
+#cp -R desmume-ubuntu-debian/debian/* desmume-release_0_9_13/debian/
+cp -R desmume-23de6bb7b34c5f941eaa8ec19c89eb83df414ecd/debian/* desmume-release_0_9_13/debian/
+#rm -rf desmume-ubuntu-debian
+rm -f desmume-23de6bb7b34c5f941eaa8ec19c89eb83df414ecd
 cd desmume-release_0_9_13
 debuild
