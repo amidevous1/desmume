c53867c2ae3492143c3ee5964b83fa0c1486854d
amidevous1 <amidevous@gmail.com>
2025-06-08 20:51:02 +0200

    Update create-deb-file-ubuntu-debian.sh

--- create-deb-file-ubuntu-debian.sh
+++ create-deb-file-ubuntu-debian.sh
@@ -17,15 +17,12 @@ sudo apt-get -y install libsoundtouch-dev
 sudo apt-get -y install libtinyxml-dev
 rm -f desmume*
 wget https://github.com/TASEmulators/desmume/archive/refs/tags/release_0_9_13.tar.gz -O desmume_0.9.13.orig.tar.gz
-#wget https://archive.ubuntu.com/ubuntu/pool/universe/d/desmume/desmume_0.9.13.orig.tar.gz -O desmume_0.9.13.orig.tar.gz
 tar -xvf desmume_0.9.13.orig.tar.gz
-#wget https://github.com/amidevous1/desmume/archive/refs/heads/ubuntu-debian.tar.gz -O desmume_0.9.13-4.debian.tar.gz
-wget https://archive.ubuntu.com/ubuntu/pool/universe/d/desmume/desmume_0.9.13-4.debian.tar.xz -O desmume_0.9.13-4.debian.tar.gz
-#tar -xvf desmume_0.9.13-4.debian.tar.gz
-#rm -f desmume_0.9.13-4.debian.tar.gz
-#mkdir desmume-release_0_9_13/debian
-#cp -R desmume-ubuntu-debian/debian/* desmume-release_0_9_13/debian/
-#rm -rf desmume-ubuntu-debian
+wget https://github.com/amidevous1/desmume/archive/refs/heads/ubuntu-debian.tar.gz -O desmume_0.9.13-4.debian.tar.gz
+tar -xvf desmume_0.9.13-4.debian.tar.gz
+rm -f desmume_0.9.13-4.debian.tar.gz
+mkdir desmume-release_0_9_13/debian
+cp -R desmume-ubuntu-debian/debian/* desmume-release_0_9_13/debian/
+rm -rf desmume-ubuntu-debian
 cd desmume-release_0_9_13
-tar -xvf ../desmume_0.9.13-4.debian.tar.gz
 debuild
