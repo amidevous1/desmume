4e28208aa33abaed7f1b8d009eee7fc0eeb79a78
amidevous1 <amidevous@gmail.com>
2025-06-08 20:53:58 +0200

    Update create-deb-file-ubuntu-debian.sh

--- create-deb-file-ubuntu-debian.sh
+++ create-deb-file-ubuntu-debian.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y dist-upgrade
-sudo apt-get -y install build-essential gettext intltool debhelper cdbs lintian fakeroot devscripts dh-make dput git tar xz-utils gzip
+sudo apt-get -y install software-properties-common
+# force use debhelper version 13 for old ubuntu version (18.04,20.04)
+sudo add-apt-repository ppa:andykimpe/debhelper-13 -y
+sudo apt update
+sudo apt-get -y install build-essential gettext intltool debhelper cdbs lintian fakeroot devscripts dh-make dput git tar xz-utils gzip wget
+sudo apt-get -y install debhelper
+sudo apt-get -y install meson
+sudo apt-get -y install cmake
+sudo apt-get -y install libglib2.0-dev
 sudo apt-get -y install libsdl2-dev
 sudo apt-get -y install libpcap-dev
-sudo apt-get -y install libgtk2.0-dev
-sudo apt-get -y install libgtk-3-dev
-sudo apt-get -y install libsoundtouch-dev
-sudo apt-get -y install libagg-dev
 sudo apt-get -y install libopenal-dev
-sudo apt-get -y install libgl-dev
-sudo apt-get -y install libgles2-mesa-dev
-sudo apt-get -y install libgles-dev
-sudo apt-get -y install libgl1-mesa-dev libgl1-mesa-dri libglx-mesa0 libglx0
-sudo apt-get -y install libglx-dev
-sudo apt-get -y install libegl-mesa0 libegl1 libegl1-mesa-dev
-libegl-dev
-wget https://github.com/TASEmulators/desmume/archive/refs/tags/release_0_9_11.tar.gz
-tar -xvf release_0_9_11.tar.gz
-cd release_0_9_11/desmume
-mv src desmume-0_9_11
+sudo apt-get -y install libsoundtouch-dev
+sudo apt-get -y install libtinyxml-dev
+rm -f desmume*
+wget https://github.com/TASEmulators/desmume/archive/refs/tags/release_0_9_13.tar.gz -O desmume_0.9.13.orig.tar.gz
+tar -xvf desmume_0.9.13.orig.tar.gz
+wget https://github.com/amidevous1/desmume/archive/refs/heads/ubuntu-debian.tar.gz -O desmume_0.9.13-4.debian.tar.gz
+tar -xvf desmume_0.9.13-4.debian.tar.gz
+rm -f desmume_0.9.13-4.debian.tar.gz
+mkdir desmume-release_0_9_13/debian
+cp -R desmume-ubuntu-debian/debian/* desmume-release_0_9_13/debian/
+rm -rf desmume-ubuntu-debian
+cd desmume-release_0_9_13
+debuild
